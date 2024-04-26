2af518f73642d2700197e310f7bc9618ab2b0609
Ansor080 <1000cpsbw@gmail.com>
2024-04-26 20:55:27 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+let alphabet_reversed = "";
+
+let alphabet_arr = [
+    {
+        idx: 1,
+        leter: "A"
+    }
+]
+
+for (let i = 0; i < alphabet.length; i++) {
+    alphabet_arr.push({
+        idx: i + 1,
+        leter: alphabet[i]
+    });
+}
+
+for (let i = alphabet.length - 1; i >= 0; i--) {
+    alphabet_reversed += alphabet[i];
+}
+
+console.log(alphabet_arr);
+console.log(alphabet_reversed);
